132f762c3afedf0bdf89cfe72226793dedacba20
Anup96 <anup9922jadhav>
2022-01-16 16:04:13 +0530

    Hello

---
+++ src/main/java/com/base/BaseClass.java
@@ -0,0 +1,35 @@
+package com.base;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+import com.reports.Extentreport;
+import com.utility.Constants;
+
+public class BaseClass extends  Extentreport{
+	public static WebDriver driver;
+	public static WebDriver initializeDriver(String browser){
+		
+	//	try {
+			driver = BrowerFactory.browserInstance(browser);
+			driver.get(ReadPropertyFile.get("url"));
+			//DriverManager.setWebDriver(driver);
+			driver.manage().window().maximize();
+			//driver.manage().deleteAllCookies();
+			//driver.manage().timeouts().pageLoadTimeout(Constants.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
+			//driver.manage().timeouts().implicitlyWait(Constants.IMPLICIT_WAIT, TimeUnit.SECONDS);
+		//} catch (Exception e) {
+		//	e.printStackTrace();
+	//}
+		return driver;
+		
+		
+		
+	}
+	
+	
+	
+}
